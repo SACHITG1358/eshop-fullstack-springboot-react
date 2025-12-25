0d61b1ae2a35feff4de72c1709ec0d73adf657d3
SACHITG1358 <sachitg1358@gmail.com>
2025-12-26 04:37:52 +0530

    Initial commit: full-stack e-commerce application

---
+++ src/main/java/com/eshop/resaleapp/ResaleappApplication.java
@@ -0,0 +1,13 @@
+package com.eshop.resaleapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResaleappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResaleappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eshop/resaleapp/ResaleappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eshop.resaleapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResaleappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
